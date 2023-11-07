2bf259d620ba107391ef11bc3d51fc3a27ddda8f
Tristan <tristan.chardes@gmail.com>
2023-11-07 11:35:14 +0100

    le debupanel fonctionne bien

--- app/mods/sunspec_debug_panel/src/get.py
+++ app/mods/sunspec_debug_panel/src/get.py
@@ -55,7 +55,7 @@ def mergeDico(dicoDocu, dicoOnduleur, pointOrigin):
 # fonction pour récupérer un model sous la forme d'un dico mergé
 def getModel(dicoOrigin, numModel):
     # /!\ changer chemin pour que l'import marche de nimport ou
-    with open(f"./sunData/models/json/model_{numModel}.json") as jsonFile:
+    with open(f"./mods/sunspec_debug_panel/src/models/json/model_{numModel}.json") as jsonFile:
         dicoModel = json.load(jsonFile)
     pointOrigin = dicoOrigin
     dicoOrigin = dicoOrigin.get_dict(computed=True)
