2630451d122f2f58b70ac2ed3e80ae9dc368c06c
Tristan <tristan.chardes@gmail.com>
2023-11-07 10:14:55 +0100

    merge en crlt c de l'obkect qui contient les onduleurs

--- app/mods/onduleur/main.py
+++ app/mods/onduleur/main.py
@@ -5,3 +5,6 @@ from .src.Onduleurs_data import Onduleurs_data, Onduleurs
 if not memo["onduleurs_data"]:
 	memo["onduleurs_data"] = Onduleurs_data()
 	Onduleurs().start()
+
+memo["onduleurs_data"].ajouterOnduleurs()
+print(memo["onduleurs_data"].execOnduleur("d8-10-9f-db-84-b3", "getNom"))
--- app/mods/onduleur/src/_Onduleurs.py
+++ app/mods/onduleur/src/_Onduleurs.py
@@ -1,24 +1,31 @@
-from subprocess import Popen, PIPE, check_output
+from subprocess import check_output, run
 import re
-IP = "1.2.3.4"
+from .onduleur import OnduleurSunspec, OnduleurHuawei
 
-# do_ping(IP)
-# The time between ping and arp check must be small, as ARP may not cache long
+APR_CMD = "a"
+ARP_MAC_SEP = "-"
 
 class _Onduleurs():
     def __init__(self):
         self.onduleurs = {}
-    
 
-    def decouvreOnduleurs(self):
+    def ajouterOnduleurs(self):
         onduleurHardcoder = {
-            "192.168.200.1" : 6607
+            "192.168.100.161" : {
+                "port" : 6607,
+                "type" : OnduleurHuawei,
+                "slave_id" : 0,
+                "utilisateur" : "instaler",
+                "mdp" : "Emeraude7850"
+                }
         }
-        for onduleur in onduleurHardcoder.items():
-            check_output("ping" + onduleur)
+        for key, value in onduleurHardcoder.items():
+            check_output("ping -n 1 " + key)
+            res = run(["arp", "-"+APR_CMD, key], capture_output=True, text=True)
+            p = re.compile(r'([0-9a-f]{2}(?:'+ARP_MAC_SEP+'[0-9a-f]{2}){5})', re.IGNORECASE)
+            mac = re.findall(p, res.stdout)[0]
+            self.onduleurs[mac] = value["type"](key, value["port"], slaveId=value["slave_id"], utilisateur=value["utilisateur"], mdp=value["mdp"])
+        print(self.onduleurs)
 
-            check_output()
-            
-            # pid = Popen(["arp", "-n", IP], stdout=PIPE)
-            # s = pid.communicate()[0]
-            # mac = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", s).groups()[0]
+    def execOnduleur(self, mac, cmd, *args, **kwargs):
+        return getattr(self.onduleurs[mac], cmd)(*args, **kwargs)
