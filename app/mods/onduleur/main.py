7a911b5cc8f1c422a61075213d06ebcfdafd2079
Tristan <tristan.chardes@gmail.com>
2023-11-07 16:25:01 +0100

    la fin de la journée

--- app/mods/onduleur/main.py
+++ app/mods/onduleur/main.py
@@ -6,7 +6,7 @@ if not memo["onduleurs_data"]:
 	memo["onduleurs_data"] = Onduleurs_data()
 	Onduleurs().start()
 
-# memo["onduleurs_data"].ajouterOnduleurs()
+memo["onduleurs_data"].ajouterOnduleurs()
 # import time
 # time.sleep(10)
 # print(memo["onduleurs_data"].execOnduleur("d8-10-9f-db-84-b3", "getNom"))
--- app/mods/onduleur/src/_Onduleurs.py
+++ app/mods/onduleur/src/_Onduleurs.py
@@ -1,6 +1,7 @@
 from subprocess import check_output, run
 import re
 from .onduleur import OnduleurSunspec, OnduleurHuawei
+from src.mysql_src import mysqlite
 
 APR_CMD = "a"
 ARP_MAC_SEP = "-"
@@ -25,7 +26,11 @@ class _Onduleurs():
             p = re.compile(r'([0-9a-f]{2}(?:'+ARP_MAC_SEP+'[0-9a-f]{2}){5})', re.IGNORECASE)
             mac = re.findall(p, res.stdout)[0]
             self.onduleurs[mac] = value["type"](key, value["port"], slaveId=value["slave_id"], utilisateur=value["utilisateur"], mdp=value["mdp"])
-        print(self.onduleurs)
+            
+            res = mysqlite.exec("select mac from onduleur where mac='" + mac + "'")
+            print(res)
+            if res == []:
+                mysqlite.exec("insert into onduleur (nom, id_centrale, mac, type) values (?, ?, ?, ?)", (self.onduleurs[mac].getNom(), 0, mac, value["type"].__name__))
 
     def execOnduleur(self, mac, cmd, *args, **kwargs):
         return getattr(self.onduleurs[mac], cmd)(*args, **kwargs)
