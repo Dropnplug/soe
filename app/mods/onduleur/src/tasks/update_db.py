d59d0b36c67cac9724cc60e0443e459035808743
Dropnplug <dropbird.rennes@gmail.com>
2023-11-10 08:44:56 +0100

    Update update_db.py

--- app/mods/onduleur/src/tasks/update_db.py
+++ app/mods/onduleur/src/tasks/update_db.py
@@ -1,7 +1,6 @@
 from src.memo import memo
 
 def update_db():
-    if memo["onduleurs_data"].isReady():
-        memo["onduleurs_data"].majAllDataBdd(_timeout=100)
-    else:
+    if not memo["onduleurs_data"].isReady():
         memo["onduleurs_data"].ajouterOnduleurs(_timeout=10)
+    memo["onduleurs_data"].majAllDataBdd(_timeout=100)
