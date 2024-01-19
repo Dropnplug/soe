a6ad48f1a3ef7c9993dd38f9914f45185c2aa911
Tristan <tristan.chardes@gmail.com>
2024-01-19 16:20:43 +0100

    tada

---
+++ app/mods/soe/web/pages/rapport/rapport.py
@@ -0,0 +1,9 @@
+import flask as fl
+from flask import request
+from src.flask_src.app import app
+from src.flask_src.deco import inspect_session
+
+@app.route('/soe/rapport/', methods=['GET'])
+@inspect_session([], redirect_url="/soe/logout/")
+def dashboard_soe():
+    return fl.render_template('/mods/soe/pages/rapport/rapport.html')
