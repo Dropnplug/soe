345e0b4f3ee6ce7be6cebe120fd9ddc911010f0f
Tristan <tristan.chardes@gmail.com>
2024-02-15 16:24:38 +0100

    le multi grpahique a des input pour choisir les données à affichées

--- app/mods/soe/web/pages/rapport/rapport.js
+++ app/mods/soe/web/pages/rapport/rapport.js
@@ -1,4 +1,31 @@
-function requestDataMultiChart() {
+const listeData = {
+    "Puissance DC": (data) => {console.log(data)},
+    "Tension DC": (event) => {console.log(event)},
+    "Courant DC": (event) => {console.log(event)},
+    "Puissance AC ": (event) => {console.log(event)},
+    "Puissance AC L1": (event) => {console.log(event)},
+    "Puissance AC L2": (event) => {console.log(event)},
+    "Puissance AC L3": (event) => {console.log(event)},
+    "Tension AC": (event) => {console.log(event)},
+    "Tension AC L1": (event) => {console.log(event)},
+    "Tension AC L2": (event) => {console.log(event)},
+    "Tension AC L3": (event) => {console.log(event)},
+    "Courant AC": (event) => {console.log(event)},
+    "Courant AC L1": (event) => {console.log(event)},
+    "Courant AC L2": (event) => {console.log(event)},
+    "Courant AC L3": (event) => {console.log(event)},
+    "Fréquence AC": (event) => {console.log(event)},
+    "Fréquence AC L1": (event) => {console.log(event)},
+    "Fréquence AC L2": (event) => {console.log(event)},
+    "Fréquence AC L3": (event) => {console.log(event)},
+    "Facteur de limitation de puissance": (event) => {console.log(event)},
+    "Déphasage cos phi": (event) => {console.log(event)},
+    "Température": (event) => {console.log(event)},
+    "Puissance réactive": (event) => {console.log(event)},
+    "Énergie": (event) => {console.log(event)},
+}
+
+function requestDataMultiChart(premiereFois=false) {
     // recup value des inputs
     let inputDateDebut = document.getElementById("inputDateDebut")
     let inputDateFin = document.getElementById("inputDateFin")
@@ -17,12 +44,10 @@ function requestDataMultiChart() {
     if (inputDateDebut.valueAsDate) {
         inputDateDebut.valueAsDate.setDate(inputDateDebut.valueAsDate.getDate()+1)
         start = inputDateDebut.valueAsDate.setHours(0, 0, 0, 0)
-        // start = inputDateDebut.valueAsNumber
     }
     if (inputDateFin.valueAsDate) {
         inputDateFin.valueAsDate.setDate(inputDateFin.valueAsDate.getDate()+1)
         end = inputDateFin.valueAsDate.setHours(0, 0, 0, 0)
-        // end = inputDateFin.valueAsNumber
     }
 
     // on retire les millisecondes
@@ -34,33 +59,138 @@ function requestDataMultiChart() {
     })
 }
 
-function creerMultiChart(data, start, end) {
-    console.log(data, start, end)
-    
+function repartirDonneesParOnudleurs(data) {
+    let onduleursData = {}
+    for (donnee in data) {
+        if (onduleursData[data[donnee]["mac_onduleur"]+"_"+data[donnee]["slave_id"]] === undefined) {
+            onduleursData[data[donnee]["mac_onduleur"]+"_"+data[donnee]["slave_id"]] = []
+        }
+        onduleursData[data[donnee]["mac_onduleur"]+"_"+data[donnee]["slave_id"]].push(data[donnee])
+    }
+    return onduleursData
 }
 
-function prout(params) {
-    let end
-    let start
-    let now = new Date()
-    // le 30 dernier jour
-    if (listeMois[now.getMonth()] == inputHistoMois.value && inputHistoAnnee.value == now.getFullYear() ) {
-        end = new Date()
-        start = new Date(now.setMonth(now.getMonth()-1))
-    // le mois selectioné
+function checkCheckbox(details) {
+        let elems = details.getElementsByTagName("input")
+        elems = [...elems]
+        elems.shift()
+        for (let elem in elems) {
+            elems[elem].disabled = !elems[elem].disabled
+        }
+}
+
+function creerInputs(onduleursData) {
+    let spanInputs = document.getElementsByClassName("inputsData")[0]
+    spanInputs.innerHTML = ""
+    const nomSite = "Site"
+
+    // le site
+    if (Object.keys(onduleursData).length != 0) {
+        let detail = document.createElement("details")
+        let summary = document.createElement("summary")
+        let label = document.createElement("label")
+        let checkbox = document.createElement("input")
+        label.setAttribute("for", nomSite)
+        label.innerText = nomSite
+        checkbox.onchange = () => checkCheckbox(detail)
+        checkbox.id = nomSite
+        checkbox.style.transform = "scale(1.3)"
+        checkbox.type = "checkbox"
+        checkbox.checked = true
+        summary.innerText = nomSite
+        summary.style.fontSize = "1.2em"
+    
+        label.appendChild(checkbox)
+        summary.appendChild(label)
+        detail.appendChild(summary)
+        for (let data in listeData) {
+            let checkbox = document.createElement("input")
+            let label = document.createElement("label")
+            label.innerText = data
+            label.setAttribute("for", data)
+            checkbox.type = "checkbox"
+            checkbox.name = data
+            checkbox.id = data
+            checkbox.onclick = () => listeData[data](onduleursData)
+            label.appendChild(checkbox)
+            detail.appendChild(label)
+        }
+        spanInputs.appendChild(detail)
     } else {
-        let mois = listeMois.indexOf(inputHistoMois.value)+1
-        let annee = Number(inputHistoAnnee.value)
-        start = new Date(annee, mois-1)
-        end = new Date(annee, mois-1, new Date(annee, mois, 0).getDate())
+        let h2 = document.createElement("h2")
+        h2.innerText = "Pas de données pour la période sélèctionnée."
+        spanInputs.appendChild(h2)
     }
-    
-    start.setDate(start.getDate() - 1)
-    
-    start = toIsoString(start).substring(0, 10)
-    end = toIsoString(end).substring(0, 10)
-    
-    request("POST", "/soe/site/dataHisto/", {"start":start, "end":end}).then(data => {
-        creerAffichageHistoriqueAC(data, start, end)
-    })
+
+    // les onduleurs
+    for (let macOnduleur in onduleursData) {
+        let detail = document.createElement("details")
+        let summary = document.createElement("summary")
+        let label = document.createElement("label")
+        let checkbox = document.createElement("input")
+        let span = document.createElement("span")
+
+        span.classList.add("spanLabelInput")
+        label.setAttribute("for", macOnduleur)
+        label.innerText = macOnduleur
+        checkbox.onchange = () => checkCheckbox(detail)
+        checkbox.id = macOnduleur
+        checkbox.style.transform = "scale(1.3)"
+        checkbox.type = "checkbox"
+        checkbox.checked = true
+        summary.innerText = "Onduleur"
+        summary.style.fontSize = "1.2em"
+
+        label.appendChild(checkbox)
+        summary.appendChild(label)
+        detail.appendChild(summary)
+        
+        // un onduleur
+        for (let data in listeData) {
+            let checkbox = document.createElement("input")
+            let label = document.createElement("label")
+            label.innerText = data
+            label.setAttribute("for", macOnduleur + "_" + data)
+            checkbox.type = "checkbox"
+            checkbox.name = data
+            checkbox.id = macOnduleur + "_" + data
+            checkbox.onclick = () => listeData[data](onduleursData[macOnduleur])
+            label.appendChild(checkbox)
+            detail.appendChild(label)
+        }
+        spanInputs.appendChild(detail)
+    }
+}
+
+function creerMultiChart(data, start, end) {
+    let onduleursData = repartirDonneesParOnudleurs(data)
+    creerInputs(onduleursData)
+
+    // update l'abscice donc le temps pck cette fonction est appellée à chaque changement de delta temps
+
+    // resolve les inputs qui sont checkés pour qu'ils se raclculent
+
+    let chart = Chart.getChart("canvasMultiChart")
+    if (!chart) {
+        let canvasMultiChart = document.getElementById("canvasMultiChart")
+        new Chart(canvasMultiChart, {
+            data: {
+                datasets: [{
+                    type: 'bar',
+                    label: 'Bar Dataset',
+                    data: []
+                }],
+                labels: []
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        display: false,
+                    }
+                }
+            }
+        })
+    } else {
+        chart.update()
+    }
 }
